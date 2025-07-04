15fb22b80d61ffec824b48ecf1285773bfdacd51
Achala Imesh Batuwangala <achalaimesh99@gmail.com>
2025-07-04 07:01:52 +0530

    feat: update

---
+++ src/constants/permissions.constants.ts
@@ -0,0 +1,12 @@
+export const PERMISSIONS = {
+  USER_MANAGEMENT: "User management",
+  ROLE_MANAGEMENT: "Role management",
+
+};
+
+export const PERMISSIONS_LIST = [
+  {
+    name: "User management",
+    permissions: [PERMISSIONS.USER_MANAGEMENT, PERMISSIONS.ROLE_MANAGEMENT],
+  },
+];
---
+++ src/routes/v1/role.route.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import roleController from "../../controllers/role.controller";
+import authMiddleware from "../../middlewares/auth.middleware";
+import checkRole from "../../middlewares/role.middleware";
+
+const router = express.Router();
+
+// Protect all routes with authentication
+router.use(authMiddleware);
+
+// Only admin can manage roles
+router.post("/", checkRole(["manage_roles"]), (req, res, next) => {
+  roleController.createRole(req, res).catch(next);
+});
+
+router.get("/", checkRole(["view_roles"]), (req, res, next) => {
+  roleController.getAllRoles(req, res).catch(next);
+});
+
+router.get("/:roleId", checkRole(["view_roles"]), (req, res, next) => {
+  roleController.getRoleById(req, res).catch(next);
+});
+
+router.put("/:roleId", checkRole(["manage_roles"]), (req, res, next) => {
+  roleController.updateRole(req, res).catch(next);
+});
+
+router.delete("/:roleId", checkRole(["manage_roles"]), (req, res, next) => {
+  roleController.deleteRole(req, res).catch(next);
+});
+
+export default router;
